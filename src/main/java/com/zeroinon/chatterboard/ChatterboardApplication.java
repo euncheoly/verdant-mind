258a1b52263363ed8876273dba9d50df6188203c
euncheol <charliekk09@naver.com>
2024-04-10 17:27:21 +0900

    project initialize

---
+++ src/main/java/com/zeroinon/chatterboard/ChatterboardApplication.java
@@ -0,0 +1,13 @@
+package com.zeroinon.chatterboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatterboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatterboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeroinon/chatterboard/ChatterboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeroinon.chatterboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatterboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
