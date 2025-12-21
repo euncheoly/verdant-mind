c91a6703b7d7db2b608b6f24f2fea88588d99e71
euncheol <charliekk09@naver.com>
2025-12-21 17:15:31 +0900

    ci test w jenkins

--- src/main/java/com/zeroinon/chatterboard/ChatterboardApplication.java
+++ src/main/java/com/zeroinon/chatterboard/ChatterboardApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChatterboardApplication {
 
 	public static void main(String[] args) {
+		System.out.println("############ CI TEST @" + System.currentTimeMillis() + "############");
 		SpringApplication.run(ChatterboardApplication.class, args);
 	}
 
